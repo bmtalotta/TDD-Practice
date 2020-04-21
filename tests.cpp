80964d103bc667d470fd17ce74c70e95fc313ab1
Ben Talotta <bmtalotta@alaska.edu>
2020-04-20 20:09:51 -0800

    passing newline deliminated test

--- tests.cpp
+++ tests.cpp
@@ -20,5 +20,11 @@ TEST_CASE("single number returns same value"){
 }
 TEST_CASE("two numbers comma deliminated returns the sum"){
     CHECK(stringCalculator("1,1") == 2);
+    CHECK(stringCalculator("8,2") == 10);
+    CHECK(stringCalculator("4,1") == 5);
 }
-
+TEST_CASE("two numbers newline deliminated returns the sum"){
+    CHECK(stringCalculator("1\n1") == 2);
+    CHECK(stringCalculator("8\n2") == 10);
+    CHECK(stringCalculator("4\n1") == 5);
+}
